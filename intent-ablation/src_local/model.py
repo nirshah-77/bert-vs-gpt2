5dcf4c109ddff3f7e2726b3134c19eb93816d281
nirshah-77 <nirshah0209@gmail.com>
2026-08-18 16:18:46 +0530

    removed the BERT pooler

--- intent-ablation/src_local/model.py
+++ intent-ablation/src_local/model.py
@@ -1,12 +1,87 @@
-from transformers import AutoModelForSequenceClassification
+import torch
+import torch.nn as nn
+from transformers import AutoModelForSequenceClassification, BertForSequenceClassification
+from transformers.modeling_outputs import SequenceClassifierOutput
 from peft import get_peft_model, LoraConfig, TaskType
 
 from config import MODEL_NAMES, NUM_LABELS, LORA_CONFIG
 
 
+class CustomBertForSequenceClassification(BertForSequenceClassification):
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        # Bypassing the implicit pooler (outputs[1]) and extracting the raw [CLS] token representation
+        cls_representation = outputs.last_hidden_state[:, 0, :]
+
+        pooled_output = self.dropout(cls_representation)
+        logits = self.classifier(pooled_output)
+
+        loss = None
+        if labels is not None:
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = nn.MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = nn.CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = nn.BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+
 def build_model(model_key, strategy):
     model_name = MODEL_NAMES[model_key]
-    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=NUM_LABELS)
+    if model_key == "bert":
+        model = CustomBertForSequenceClassification.from_pretrained(model_name, num_labels=NUM_LABELS)
+    else:
+        model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=NUM_LABELS)
 
     if model_key == "gpt2":
         from transformers import AutoTokenizer
